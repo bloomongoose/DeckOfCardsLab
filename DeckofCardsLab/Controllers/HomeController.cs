4bba3fc11099c4319294116a465f5562c6948352
radeen <radeen013@gmail.com>
2021-11-03 09:43:00 -0400

    reshuffling

--- DeckofCardsLab/Controllers/HomeController.cs
+++ DeckofCardsLab/Controllers/HomeController.cs
@@ -30,10 +30,23 @@ namespace DeckofCardsLab.Controllers
             return View();
         }
 
-        public IActionResult Deck()
+        public IActionResult CreateDeck()
         {
-            DeckModel result = deckDAL.GetData();
-            return View();
+            DeckModel result = deckDAL.GetDeck();
+            deckDAL.ShuffleDeck(result.deck_id);
+            HttpContext.Response.Cookies.Append("DeckID", result.deck_id);
+            return RedirectToAction("Hand");
+        }
+
+        public IActionResult Hand()
+        {
+            string deckid = HttpContext.Request.Cookies["DeckID"];
+            HandModel result = deckDAL.GetHand(deckid);
+            if(result.remaining < 5)
+            {
+                deckDAL.ShuffleDeck(deckid);
+            }
+            return View(result);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- DeckofCardsLab/Models/DeckDAL.cs
+++ DeckofCardsLab/Models/DeckDAL.cs
@@ -10,7 +10,7 @@ namespace DeckofCardsLab.Models
 {
     public class DeckDAL
     {
-        public DeckModel GetData()
+        public DeckModel GetDeck()
         {
             string url = $"https://deckofcardsapi.com/api/deck/new/";
 
@@ -26,5 +26,33 @@ namespace DeckofCardsLab.Models
 
             return result;
         }
+
+        public void ShuffleDeck(string id)
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{id}/shuffle//";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        }
+
+        public HandModel GetHand(string id)
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{id}/draw/?count=5";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            HandModel result = JsonConvert.DeserializeObject<HandModel>(JSON);
+
+            return result;
+        }
     }
+
 }
